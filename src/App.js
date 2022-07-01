fe3a15d04537069384d3d168285caf44cf3c653c
Cosmin <cosminmvp08@gmail.com>
2022-07-01 19:03:23 +0300

    Added loging menu(testing)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  
+  const adminUser = {
+    email: "admin@admin.ro",
+    password: "duckoff"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      setError("Go away!");
+    }
+  }
+
+  const Logout = () => {
+    setUser({
+      name: '',
+      email: ''
+    });
+    console.log("Logout");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and aaaaaaaaaaaaaaaaaa to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className='welcome'>
+          <h2>welcome <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) :
+      (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
