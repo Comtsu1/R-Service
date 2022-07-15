748c1358d186fd9e9363bcdcc2a79e293db2cec2
Comtsu1 <cosminmvp08@gmail.com>
2022-07-15 13:40:31 +0300

    added connection to backend in the loginForm, with axios.

--- frontend/src/Pages/Login/components/LoginForm.js
+++ frontend/src/Pages/Login/components/LoginForm.js
@@ -2,44 +2,45 @@ import React, {useState} from "react";
 import './LoginForm.css';
 import visibility from "../Icons/visibility.png";
 import unVisibility from "../Icons/visibility_slash.png";
+import axios from 'axios';
 
 class ForgotPassword extends React.Component{
     constructor(props){
-        super(props);
-        this.state = {
-            email: '',
-        }
+	super(props);
+	this.state = {
+	    email: '',
+	}
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event){
-        if(this.state.email){
-            console.log("submitted password reset")
-        }
-        event.preventDefault();
+	if(this.state.email){
+	    console.log("submitted password reset")
+	}
+	event.preventDefault();
     }
 
     handleChange(event){
-        this.setState({email: event.target.value})
+	this.setState({email: event.target.value})
     }
 
 
     render(){
-        return(
-            <div className='Wrapper'>
-                <h1>
-                    Reset Password
-                </h1>
-                <p id='Forgot-description'>
-                    Please enter your email address and we'll send you a link to reset your password. <br/> --false
-                </p>
-                <form id="ForgotForm" onSubmit={this.handleSubmit}>
-                    <input placeholder="Enter your email" type='email' value={this.state.email} onChange={this.handleChange}></input>
-                </form>
-            </div>
-        )
+	return(
+	    <div className='Wrapper'>
+	    <h1>
+	    Reset Password
+	    </h1>
+	    <p id='Forgot-description'>
+	    Please enter your email address and we'll send you a link to reset your password. <br/> --false
+	    </p>
+	    <form id="ForgotForm" onSubmit={this.handleSubmit}>
+	    <input placeholder="Enter your email" type='email' value={this.state.email} onChange={this.handleChange}></input>
+	    </form>
+	    </div>
+	)
     }
 
 }
@@ -48,111 +49,124 @@ function LoginForm({Login, error}) {
     const [details, setDetails] = useState({name: "", email: "", password: "", PasswordTextIsShown: false, ForgotPass: false});
 
     const submitHandler = e => {
-        e.preventDefault();
+	e.preventDefault();
 
-        Login(details);
+	Login(details);
     }
 
     function toggleText(){
-        setDetails({...details, PasswordTextIsShown: !details.PasswordTextIsShown})
+	setDetails({...details, PasswordTextIsShown: !details.PasswordTextIsShown})
     }
 
     function ToggleForgotPasswordForm(){
-        setDetails({...details, ForgotPass: !details.ForgotPass})
-        console.log(details.ForgotPass)
+	setDetails({...details, ForgotPass: !details.ForgotPass})
+	console.log(details.ForgotPass)
     }
 
     function renderToggleButton(){
-        if(details.password){
-            return(
-                <button
-                id="toggleText"
-                type="button"
-                onClick={toggleText}
-                >
-                    <img src={details.PasswordTextIsShown?unVisibility:visibility}
-                    className = "eye-image"
-                    alt={details.PasswordTextIsShown?"eye-slashed":"eye"}
-                    ></img>
-                </button>
-            );
-        }
+	if(details.password){
+	    return(
+		<button
+		id="toggleText"
+		type="button"
+		onClick={toggleText}
+		>
+		<img src={details.PasswordTextIsShown?unVisibility:visibility}
+		className = "eye-image"
+		alt={details.PasswordTextIsShown?"eye-slashed":"eye"}
+		></img>
+		</button>
+	    );
+	}
     }
 
     function textPassword(){
-        if(!details.password && details.PasswordTextIsShown){
-            setDetails({...details, PasswordTextIsShown: false})
-        }
-        if(details.PasswordTextIsShown){
-            return("text")
-        }else{
-            return("password")
-        }
+	if(!details.password && details.PasswordTextIsShown){
+	    setDetails({...details, PasswordTextIsShown: false})
+	}
+	if(details.PasswordTextIsShown){
+	    return("text")
+	}else{
+	    return("password")
+	}
+    }
+
+    function sendDataDB(){
+	const payload = {
+	    username: "IchWill",
+	    email:details.email,
+	    password:details.password
+	};
+
+	axios.post('http://localhost:8080/user/login', payload)
+	.then(res => console.log("bruh: " + res.status))
+		.catch(err => console.log("Error status: " + err.response.status + "\n data: " + err))
+
     }
 
 
     if(details.ForgotPass){
-        return (
-            <>
-                <div className="Wrapper-bg">
-                    <div className="Wrapper">
-                        <ForgotPassword/>
-                        <div className="Footer">
-                            <button type="button" onClick={ToggleForgotPasswordForm}>
-                                return
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
+	return (
+	    <>
+	    <div className="Wrapper-bg">
+	    <div className="Wrapper">
+	    <ForgotPassword/>
+	    <div className="Footer">
+	    <button type="button" onClick={ToggleForgotPasswordForm}>
+	    return
+	    </button>
+	    </div>
+	    </div>
+	    </div>
+	    </>
+	)
     }else{
-        return (
-            <div className="Wrapper-bg">
-                <div className="Wrapper">
-                    <form onSubmit={submitHandler}>
-                        <div className="form-inner">
-                            <h2>Login</h2>
-                            {(error !== "") ? (<div className="error">{error}</div>) : ""}
-                            <div className="form-group email">
-                                <label htmlFor="email">Email: </label>
-                                <input type={'email'} name='email' id='email' onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
-                            </div>
-                            <div className="form-group password">
-                                <label htmlFor="password">Password: </label>
-                                <div className="password-placeholder">
-                                    <input 
-                                    className="password-input"
-                                    type={textPassword()} 
-                                    name='password' 
-                                    id='password' 
-                                    onChange={e => setDetails({
-                                        ...details, 
-                                        password: e.target.value})
-                                    } 
-                                    value={details.password}>
-                                    </input>
-                                    {renderToggleButton()}
-    
-                                </div>
-                            </div>
-                            <input type={'submit'} value="LOGIN" />
-                        </div>
-                    </form>
-    
-                    <div className="Footer">
-                        <button type="button" onClick={ToggleForgotPasswordForm}>
-                            Forgot Password?
-                        </button>
-                        <a href="register">
-                            <button>
-                                Register
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
+	return (
+	    <div className="Wrapper-bg">
+	    <div className="Wrapper">
+	    <form onSubmit={submitHandler}>
+	    <div className="form-inner">
+	    <h2>Login</h2>
+	    {(error !== "") ? (<div className="error">{error}</div>) : ""}
+	    <div className="form-group email">
+	    <label htmlFor="email">Email: </label>
+	    <input type={'email'} name='email' id='email' onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
+	    </div>
+	    <div className="form-group password">
+	    <label htmlFor="password">Password: </label>
+	    <div className="password-placeholder">
+	    <input 
+	    className="password-input"
+	    type={textPassword()} 
+	    name='password' 
+	    id='password' 
+	    onChange={e => setDetails({
+		...details, 
+		password: e.target.value})
+	    } 
+	    value={details.password}>
+	    </input>
+	    {renderToggleButton()}
+
+	    </div>
+	    </div>
+	    <input type={'submit'} value="LOGIN" onClick={sendDataDB}/>
+	    </div>
+	    </form>
+
+	    <div className="Footer">
+	    <button type="button" onClick={ToggleForgotPasswordForm}>
+	    Forgot Password?
+	    </button>
+	    <a href="register">
+	    <button>
+	    Register
+	    </button>
+	    </a>
+	    </div>
+	    </div>
+	    </div>
+	);
     }
 }
 
