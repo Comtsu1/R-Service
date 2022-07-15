59845b8173f2f01eea1c44ee1414f053dd24f522
Comtsu1 <cosminmvp08@gmail.com>
2022-07-15 12:39:55 +0300

    fixed indenting on LoginForm.js

--- frontend/src/Pages/Login/components/LoginForm.js
+++ frontend/src/Pages/Login/components/LoginForm.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './LoginForm.css';
 import visibility from "../Icons/visibility.png";
 import unVisibility from "../Icons/visibility_slash.png";
 import axios from 'axios';
 
-class ForgotPassword extends React.Component{
-	constructor(props){
+class ForgotPassword extends React.Component {
+	constructor(props) {
 		super(props);
 		this.state = {
 			email: '',
@@ -15,38 +15,38 @@ class ForgotPassword extends React.Component{
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	handleSubmit(event){
-		if(this.state.email){
+	handleSubmit(event) {
+		if (this.state.email) {
 			console.log("submitted password reset")
 		}
 		event.preventDefault();
 	}
 
-	handleChange(event){
-		this.setState({email: event.target.value})
+	handleChange(event) {
+		this.setState({ email: event.target.value })
 	}
 
 
-	render(){
-		return(
+	render() {
+		return (
 			<div className='Wrapper'>
-			<h1>
-			Reset Password
-			</h1>
-			<p id='Forgot-description'>
-			Please enter your email address and we'll send you a link to reset your password. <br/> --false
-			</p>
-			<form id="ForgotForm" onSubmit={this.handleSubmit}>
-			<input placeholder="Enter your email" type='email' value={this.state.email} onChange={this.handleChange}></input>
-			</form>
+				<h1>
+					Reset Password
+				</h1>
+				<p id='Forgot-description'>
+					Please enter your email address and we'll send you a link to reset your password. <br /> --false
+				</p>
+				<form id="ForgotForm" onSubmit={this.handleSubmit}>
+					<input placeholder="Enter your email" type='email' value={this.state.email} onChange={this.handleChange}></input>
+				</form>
 			</div>
 		)
 	}
 
 }
 
-function LoginForm({Login, error}) {
-	const [details, setDetails] = useState({name: "", email: "", password: "", PasswordTextIsShown: false, ForgotPass: false});
+function LoginForm({ Login, error }) {
+	const [details, setDetails] = useState({ name: "", email: "", password: "", PasswordTextIsShown: false, ForgotPass: false });
 
 	const submitHandler = e => {
 		e.preventDefault();
@@ -54,48 +54,48 @@ function LoginForm({Login, error}) {
 		Login(details);
 	}
 
-	function toggleText(){
-		setDetails({...details, PasswordTextIsShown: !details.PasswordTextIsShown})
+	function toggleText() {
+		setDetails({ ...details, PasswordTextIsShown: !details.PasswordTextIsShown })
 	}
 
-	function ToggleForgotPasswordForm(){
-		setDetails({...details, ForgotPass: !details.ForgotPass})
+	function ToggleForgotPasswordForm() {
+		setDetails({ ...details, ForgotPass: !details.ForgotPass })
 		console.log(details.ForgotPass)
 	}
 
-	function renderToggleButton(){
-		if(details.password){
-			return(
+	function renderToggleButton() {
+		if (details.password) {
+			return (
 				<button
-				id="toggleText"
-				type="button"
-				onClick={toggleText}
+					id="toggleText"
+					type="button"
+					onClick={toggleText}
 				>
-				<img src={details.PasswordTextIsShown?unVisibility:visibility}
-				className = "eye-image"
-				alt={details.PasswordTextIsShown?"eye-slashed":"eye"}
-				></img>
+					<img src={details.PasswordTextIsShown ? unVisibility : visibility}
+						className="eye-image"
+						alt={details.PasswordTextIsShown ? "eye-slashed" : "eye"}
+					></img>
 				</button>
 			);
 		}
 	}
 
-	function textPassword(){
-		if(!details.password && details.PasswordTextIsShown){
-			setDetails({...details, PasswordTextIsShown: false})
+	function textPassword() {
+		if (!details.password && details.PasswordTextIsShown) {
+			setDetails({ ...details, PasswordTextIsShown: false })
 		}
-		if(details.PasswordTextIsShown){
-			return("text")
-		}else{
-			return("password")
+		if (details.PasswordTextIsShown) {
+			return ("text")
+		} else {
+			return ("password")
 		}
 	}
 
-	function sendDataDB(){
+	function sendDataDB() {
 		const payload = {
 			username: "IchWill",
-			email:details.email,
-			password:details.password
+			email: details.email,
+			password: details.password
 		};
 
 		axios.post('http://localhost:8080/user/login', payload)
@@ -105,69 +105,70 @@ function LoginForm({Login, error}) {
 	}
 
 
-	if(details.ForgotPass){
+	if (details.ForgotPass) {
 		return (
 			<>
-			<div className="Wrapper-bg">
-			<div className="Wrapper">
-			<ForgotPassword/>
-			<div className="Footer">
-			<button type="button" onClick={ToggleForgotPasswordForm}>
-			return
-			</button>
-			</div>
-			</div>
-			</div>
+				<div className="Wrapper-bg">
+					<div className="Wrapper">
+						<ForgotPassword />
+						<div className="Footer">
+							<button type="button" onClick={ToggleForgotPasswordForm}>
+								return
+							</button>
+						</div>
+					</div>
+				</div>
 			</>
 		)
-	}else{
+	} else {
 		return (
 			<div className="Wrapper-bg">
-			<div className="Wrapper">
-			<form onSubmit={submitHandler}>
-			<div className="form-inner">
-			<h2>Login</h2>
-			{(error !== "") ? (<div className="error">{error}</div>) : ""}
-			<div className="form-group email">
-			<label htmlFor="email">Email: </label>
-			<input type={'email'} name='email' id='email' onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
-			</div>
-			<div className="form-group password">
-			<label htmlFor="password">Password: </label>
-			<div className="password-placeholder">
-			<input 
-			className="password-input"
-			type={textPassword()} 
-			name='password' 
-			id='password' 
-			onChange={e => setDetails({
-				...details, 
-				password: e.target.value})
-			} 
-			value={details.password}>
-			</input>
-			{renderToggleButton()}
+				<div className="Wrapper">
+					<form onSubmit={submitHandler}>
+						<div className="form-inner">
+							<h2>Login</h2>
+							{(error !== "") ? (<div className="error">{error}</div>) : ""}
+							<div className="form-group email">
+								<label htmlFor="email">Email: </label>
+								<input type={'email'} name='email' id='email' onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
+							</div>
+							<div className="form-group password">
+								<label htmlFor="password">Password: </label>
+								<div className="password-placeholder">
+									<input
+										className="password-input"
+										type={textPassword()}
+										name='password'
+										id='password'
+										onChange={e => setDetails({
+											...details,
+											password: e.target.value
+										})
+										}
+										value={details.password}>
+									</input>
+									{renderToggleButton()}
 
-			</div>
-			</div>
-			<input type={'submit'} value="LOGIN" onClick={sendDataDB}/>
-			</div>
-			</form>
+								</div>
+							</div>
+							<input type={'submit'} value="LOGIN" onClick={sendDataDB} />
+						</div>
+					</form>
 
-			<div className="Footer">
-			<button type="button" onClick={ToggleForgotPasswordForm}>
-			Forgot Password?
-			</button>
-			<a href="register">
-			<button>
-			Register
-			</button>
-			</a>
-			</div>
-			</div>
+					<div className="Footer">
+						<button type="button" onClick={ToggleForgotPasswordForm}>
+							Forgot Password?
+						</button>
+						<a href="register">
+							<button>
+								Register
+							</button>
+						</a>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
 
-export {LoginForm};
+export { LoginForm };
