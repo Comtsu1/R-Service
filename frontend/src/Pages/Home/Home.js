2da1577f69810d20405948032370ebefb008cafc
Temrer <temrer14915@gmail.com>
2022-09-02 20:03:21 +0300

    now changes calss on focus

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import {Footer} from './Components/Footer';
 import { Profile } from './Components/Header/Profile_Bar/Profile';
 import React , {useEffect, useState} from 'react';
 import axios from 'axios';
-import { renderMatches } from 'react-router-dom';
+import { renderMatches, resolvePath } from 'react-router-dom';
 
 function Home() {
   return (
@@ -21,6 +21,57 @@ function Home() {
 
 export {Home};
 
+
+class Post extends React.Component{
+  constructor(props){
+    super(props)
+    console.log(props)
+    this.state={
+      mouseHover: false,
+      Focused: false,
+      animated: false,
+    }
+
+    this.MouseHandler.bind(this);
+    this.MouseExit.bind(this);
+  }
+
+  MouseHandler(event){
+    this.setState({mouseHover: true})
+    setTimeout(() => {
+      if(this.state.mouseHover === true){
+        this.setState({Focused: true})
+        console.log("Succes")
+      }else{
+        console.log("exited fast")
+      }
+    }, 500);
+
+  }
+
+  MouseExit(event){
+    this.setState({mouseHover: false,
+      Focused: false,
+      animated: false,
+    })
+  }
+  
+  render(){
+    return(
+      <div key={this.props.value._id} className={this.state.Focused?"Post Focused":"Post"} onMouseEnter={(e) => this.MouseHandler(e)} onMouseLeave={(e) => this.MouseExit(e)}>
+        <div>
+          <div className='ImgWrapper'>
+            <img src={this.props.value.image[0]} alt="post Image"></img>
+          </div>
+          <a href={`/post#id=${this.props.value._id}`}>
+            <span className='Title'>{this.props.value.name}</span>
+          </a>
+        </div>
+        <span className='Cost'><br/>{this.props.value.price + " btc"}</span>
+      </div>
+    )
+    }
+}
 
 
 function MainContent(){
@@ -45,17 +96,7 @@ function MainContent(){
 
   function renderPosts(){
     const list = posts.map((value, index) => 
-      <div key={value._id} className="Post" >
-        <div>
-          <div className='ImgWrapper'>
-            <img src={value.image[0]} alt="post Image"></img>
-          </div>
-          <a href={`/post#id=${value._id}`}>
-            <span className='Title'>{value.name}</span>
-          </a>
-        </div>
-        <span className='Cost'><br/>{value.price + " btc"}</span>
-      </div>
+      <Post value={value} key={value._id}/>
     )
 
     return list
