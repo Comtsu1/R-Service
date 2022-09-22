1433ce17b7a7651e08411d46008a68bc13fee1de
Comtsu1 <cosminmvp08@gmail.com>
2022-09-22 06:19:29 +0300

    Added backend connetiong

--- frontend/src/Pages/ViewPost/Components/ReviewPopup.js
+++ frontend/src/Pages/ViewPost/Components/ReviewPopup.js
@@ -6,16 +6,35 @@ import './ReviewPopup.css'
 function ReviewPopup({Close}) {
 
     const [rating, setRating] = useState(null);
+    const [comment, setComment] = useState("");
 
     const Rate = (e, cnt) => {
         e.preventDefault();
-        // console.log(cnt);
+        if(cnt === rating) setRating(0);
         setRating(cnt);
     }
 
     const Submit = (e) => {
         e.preventDefault();
-        // axios.post(`${BackendLink}/something-todo-with-rating`)
+
+        const payload = {
+            rating: rating,
+            commnet: comment
+        };     
+
+        const config = {
+            'x-auth-token': localStorage.getItem('token')
+        };
+
+        if(config["x-auth-token"] == null) return;
+        axios.post(`${BackendLink}/review`, payload, config)
+            .then((res) => {
+                alert("Review made!")
+                Cancel();
+            })
+            .catch((err) => {
+                // handle error 
+            })
     }
 
     const Cancel = (e) => {
@@ -36,6 +55,13 @@ function ReviewPopup({Close}) {
                     <span className={rating >= 2 ? "fa fa-star focused" : "fa fa-star"} onClick={(e) => Rate(e, 2)}></span>
                     <span className={rating >= 1 ? "fa fa-star focused" : "fa fa-star"} onClick={(e) => Rate(e, 1)}></span>
                 </div>
+                {rating > 0 ?
+                <div className="comment-wrapper">
+                    <textarea className="comment-area" onChange={(e) => {setComment(e.target.value)}}></textarea>
+                </div>
+                :
+                null    
+                }
                 <button type="submit" className="submit-button" onClick={(e) => Submit(e)}>Submit</button>
                 <button type="submit" className="cancel-button" onClick={(e) => Cancel(e)}>Close</button>
             </div>
